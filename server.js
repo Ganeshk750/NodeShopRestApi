566030f8eb9dcf86592981e0f412b1e44d45e13e
Ganeshk750 <ganesh9ot@gmail.com>
2019-12-06 00:03:51 +0530

    adding product routes

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express;
-
+const app = express();
+const productRoutes = require('./api/routes/products');
 
 const port = process.env.PORT || 3000;
 
 
 app.get('/',(req,res) =>{
     res.send('Wel Come to restApi');
-})
+});
+
+
+app.use('/products',productRoutes);
 
 app.listen(port, () =>{
     console.log('Server is running on:'+ `${port}`);
