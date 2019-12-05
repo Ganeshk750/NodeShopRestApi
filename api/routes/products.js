a40d375bd90acfdd9e817c886798934683831c0c
Ganeshk750 <ganesh9ot@gmail.com>
2019-12-06 00:18:43 +0530

    adding some routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) =>{
+router.get('/',(req,res, next) =>{
     res.status(200).json({
-        message: 'Products Routes is working'
+        message: 'Products Routes with GET'
     })
+});
+
+router.post('/',(req,res,next) =>{
+    res.status(200).json({
+        message: 'Products Routes with POST'
+    })
+});
+
+router.get('/:productId',(req,res,next) =>{
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You discovered new id',
+            id: id
+        })
+    }else{
+       res.status(200).json({
+           message:'You passed an id'
+       });
+    }
 })
 
 module.exports = router;
