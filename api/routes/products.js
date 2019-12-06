0c42e03bad755e746f941bda81114dfaddb583b2
Ganeshk750 <ganesh9ot@gmail.com>
2019-12-06 13:09:32 +0530

    addind podyParser and testing

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/',(req,res, next) =>{
 });
 
 router.post('/',(req,res,next) =>{
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
     res.status(201).json({
-        message: 'Products Routes with POST'
+        message: 'Products Routes with POST',
+        created: product
     })
 });
 
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyPaser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
 
 const port = process.env.PORT || 3000;
 
+/* Its acts like logger */
 app.use(morgan('dev'));
+app.use(bodyPaser.urlencoded({extended: false}));
+app.use(bodyPaser.json());
 
-/* app.get('/',(req,res) =>{
+app.get('/',(req,res) =>{
     res.status(200).json({
         message:'Wel Come To RestApi'
     })
-}); */
+}); 
 
 
 app.use('/products',productRoutes);
 app.use('/orders',ordersRoutes);
 
 /* Handling Error */
-
 app.use((req,res,next) =>{
     const error = new Error('Not Found');
     res.status(404);
