8d34e623bd34a8430725934b81f6b9ef81ab67a5
Ganeshk750 <ganesh9ot@gmail.com>
2019-12-11 21:56:54 +0530

    addig code in orders modules and orders api

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+
 
 router.get('/',(req,res,next) =>{
     res.status(200).json({
@@ -8,13 +11,21 @@ router.get('/',(req,res,next) =>{
 });
 
 router.post('/', (req,res,next) =>{
-    const Order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order.save()
+      .exce()
+      .then(result =>{
+          console.log(result);
+          res.status(200).json({result})
+      })
+      .catch()
     res.status(201).json({
         message: 'Order was created',
-        order: Order
+        order: order
     });
 });
 
