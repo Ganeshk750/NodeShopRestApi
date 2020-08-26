7440b6eb29bdfa3f8f918f63d19043371b26238b
Ganeshk750 <ganesh9ot@gmail.com>
2020-08-26 12:58:25 +0530

    refactirng code for update product by id

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -3,38 +3,36 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
 
-
 //Multer setting
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, "./uploads/");
-    },
-    filename: function(req, file, cb) {
-      cb(
-        null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-      );
-    }
-  });
-
-  /* image file filter */
-const fileFilter = (req, file, cb) =>{
-    //reject a file
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null, true);
-    }else{
-        cb(null, false);
-    }
-}
-
-const upload = multer({
-    storage: storage, 
-    limits:{
-    fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
+/* image file filter */
+const fileFilter = (req, file, cb) => {
+  //reject a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
 
 /* GET PRODUCTS */
 exports.gets_allProducts = (req, res, next) => {
@@ -73,42 +71,43 @@ exports.gets_allProducts = (req, res, next) => {
     });
 };
 
-
 /*POST PRODUCT */
-exports.adding_product = upload.single('productImage'), (req,res,next) =>{
+(exports.adding_product = upload.single("productImage")),
+  (req, res, next) => {
     //console.log(req.file);
     const product = new Product({
-        _id:new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price,
-        /* getting productImage via multer */
-        productImage: req.file.path
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      price: req.body.price,
+      /* getting productImage via multer */
+      productImage: req.file.path,
     });
-    product.save()
-        .then(result =>{
+    product
+      .save()
+      .then((result) => {
         console.log(result);
         res.status(201).json({
-            message:'Created product successfully',
-            createdProduct: {
-                name: result.name,
-                price: result.price,
-                _id: result._id,
-                request:{
-                    type: 'GET',
-                    url: 'http://localhost:3000/products/'+ result._id
-                }
-            }
+          message: "Created product successfully",
+          createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + result._id,
+            },
+          },
         });
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({error: err})
-    });
-   /*  res.status(201).json({
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+    /*  res.status(201).json({
         message: 'Products Routes with POST',
         created: product
     }) */
-};
+  };
 
 /* GET PRODUCT BY ID */
 exports.get_orderbyId = (req, res, next) => {
@@ -146,4 +145,29 @@ exports.get_orderbyId = (req, res, next) => {
            message:'You passed an id'
        });
     } */ //This is testing static code
+};
+
+exports.update_productbyId = (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  //{name:req.body.newName, price:req.body.newPrice}//replace with updaeOps
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" + id,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
--- api/routes/products.js
+++ api/routes/products.js
@@ -12,30 +12,7 @@ router.post('/', checkAuth, ProductController.adding_product);
 router.get("/:productId", checkAuth, ProductController.get_orderbyId);
 
 
-router.patch('/:productId', checkAuth, (req,res,next) =>{
-    const id = req.params.productId;
-    const updateOps = {};
-    for(const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-    //{name:req.body.newName, price:req.body.newPrice}//replace with updaeOps
-    Product.update({_id: id},{$set:updateOps})
-       .exec()
-       .then(result =>{
-           console.log(result);
-           res.status(200).json({
-               message: 'Product updated',
-               request:{
-                   type:'GET',
-                   url: 'http://localhost:3000/products/'+ id
-               }
-           });
-       })
-       .catch(err => {
-           console.log(err);
-           res.status(500).json({error: err});
-       });
-});
+router.patch('/:productId', checkAuth, ProductController.update_productbyId);
 
 
 router.delete('/:productId', checkAuth, (req,res,next) =>{
