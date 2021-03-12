f6f29bc6bb6f5eb5821fadef89986e6e92641052
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-03-12 18:05:33 +0530

    # swagger code

--- api/models/customer.js
+++ api/models/customer.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+/**
+ * @swagger
+ * schema definition info goes here...
+ */
 const customerSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-  email:{
-      type: String,
-      required: true,
-      unique: true,
-      match:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      },
-  password:{ type: String, required: true}
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
+  password: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Customer', customerSchema); 
+module.exports = mongoose.model("Customer", customerSchema);
--- swaggerDoc.js
+++ swaggerDoc.js
@@ -2,39 +2,33 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerjsDoc = require("swagger-jsdoc");
 
 const options = {
+  explorer: true,
   swaggerDefinition: {
     info: {
       title: "E-Com Api",
       version: "1.0.0",
-      description: "E-commerce RESTful API with Swagger",
+      description: "E-commerce RESTful Endpints to test the routes",
       contact: {
         name: "DEVERLOPER",
         email: "ganeshkr04@gmail.com",
       },
     },
-    /* adding new keys */
-    swaggerOptions: {
-      authAction: {
-        JWT: {
-          name: "JWT",
-          schema: {
-            type: "token",
-            in: "header",
-            name: "Authorization",
-            description: "",
-          },
-          value: "Bearer <JWT>",
-        },
-      },
-    },
     host: "localhost:3000",
     basePath: "/",
+    securityDefinitions: {
+      bearerAuth: {
+        type: "apiKey",
+        name: "Authorization",
+        scheme: "Bearer",
+        in: "header",
+      },
+    },
   },
-  /* apis: ["./routes/*.js", "/customers.js", "/products.js", "/orders.js"], */
-  apis: ["api/routes*.js'"],
+  apis: ["./routes/*.js"],
 };
 
 const spec = swaggerjsDoc(options);
+
 module.exports = (app) => {
   app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 };
