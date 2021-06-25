e091d9ed4132259eb526d247f8ff45410f8ab03f
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-06-25 14:39:36 +0530

    # adding spec in customer and product

--- api/routes/customers.js
+++ api/routes/customers.js
@@ -4,29 +4,28 @@ const router = express.Router();
 const checkAuth = require("../middleware/check-auth");
 const customerController = require("../controllers/customer");
 
-router.post("/signup", customerController.customer_signup);
 /**
  * @swagger
- * /users/signup:
+ * /customers/signup:
  *   post:
- *      description: Used to register Customer
+ *      description: Used to register customer
  *      tags:
- *          - users
+ *          - customers
  *      parameters:
  *          - in: body
- *            name: User
- *            description: User data
+ *            name: Customer
+ *            description: Customer data
  *            schema:
  *              type: object
  *              required:
- *                 - emailId
+ *                 - email
  *                 - password
  *              properties:
- *                  emailId:
+ *                  email:
  *                      type: string
  *                      minLength: 1
  *                      maxLength: 100
- *                      example: navin@sample.com
+ *                      example: test@sample.com
  *                  password:
  *                      type: string
  *                      minLength: 1
@@ -40,9 +39,74 @@ router.post("/signup", customerController.customer_signup);
  *          '400':
  *              description: Bad request
  */
+router.post("/signup", customerController.customer_signup);
 
+/**
+ * @swagger
+ * /customers/login:
+ *   post:
+ *      description: Used to login customer
+ *      tags:
+ *          - customers
+ *      parameters:
+ *          - in: body
+ *            name: Customer
+ *            description: Customer data
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - email
+ *                 - password
+ *              properties:
+ *                  email:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 100
+ *                      example: test@sample.com
+ *                  password:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 45
+ *                      example: abcd
+ *      responses:
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.post("/login", customerController.customer_Login);
 
+/**
+ * @swagger
+ * /customers/:customerId:
+ *   delete:
+ *      description: Used to Delete customer
+ *      tags:
+ *          - customers
+ *      parameters:
+ *          - in: body
+ *            name: Customer
+ *            description: Customer Id
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - customerId
+ *              properties:
+ *                  customerId:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 100
+ *                      example: asdkjasfkdjasfh
+ *      responses:
+ *          '200':
+ *              description: Resource deleted successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.delete("/:customerId", checkAuth, customerController.customer_delete);
 
 module.exports = router;
--- swaggerDoc.js
+++ swaggerDoc.js
@@ -24,7 +24,7 @@ const options = {
       },
     },
   },
-  apis: ["./routes/*.js"],
+  apis: ["./api/routes/*.js"],
 };
 
 const spec = swaggerjsDoc(options);
